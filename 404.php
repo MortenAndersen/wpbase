4018a4367f05a41da5ef548375134a9c37574ca8
Morten Andersen <morten@hjemmesider.dk>
2016-01-19 16:46:04 +0100

    404
    add 404.php

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<?php get_sidebar( 'sidebar' ); ?>
+<article>
+<h1>Siden findes ikke - 404</h1>
+<p><strong>UNDSKYLD</strong> - siden findes desværre ikke.<br />
+Brug menuen eller gå til <a href="<?php echo esc_url( home_url( '/' ) ); ?>">forsiden</a> - <em>undskyld besværet</em>.</p>
+</article>
+<?php get_footer(); ?>
