021934f974e994f6650b2642d21226dff45e4cdd
Morten Andersen <morten@hjemmesider.dk>
2016-03-22 12:48:24 +0100

    Top menu
    only one level

--- menu-top.php
+++ menu-top.php
@@ -1,10 +1,9 @@
 <?php
 	$top_nav = array(
 		'theme_location' => 'top-menu',
+		'depth' => 1,
 		'container' =>  '',
-     	'items_wrap'  => '<nav role="navigation" class="top__menu"><ul>'."\n".'%3$s</ul></nav>',
-
-		'sub_menu' => false
+     	'items_wrap'  => '<nav role="navigation" class="top__menu"><ul>'."\n".'%3$s</ul></nav>'
 	);
 ?>
 <?php wp_nav_menu( $top_nav ); ?>
